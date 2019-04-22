1a6e19d232a4ce52601de2f90e70d1113cd24a64
travisdmathis <travisdmathis@gmail.com>
2019-04-22 02:11:16 -0400

    update config and remove dev keys

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plasmaProviderUrl: "https://mainnet-node1.leapdao.org",
   pollingDelay: 60000, // in ms
-  twilioAuthToken: '1ef31a47f2a9b23974060265680bf9e0',
-  twilioId: 'ACf13449d03655ee3b63547e1d0643094e',
-  twilioNumber: '+15184123459',
-  web3ProviderUrl: "https://mainnet.infura.io/v3/49a30c12a00f4526b012c5449e027cd6"
+  twilioAuthToken: '',
+  twilioId: '',
+  twilioNumber: '',
+  web3ProviderUrl: "https://mainnet.infura.io/v3/{yourkey}"
 };
